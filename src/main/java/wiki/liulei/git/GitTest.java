3b11798f4120029af276f2caa4b349fd3b5ee1d7
liulei <liulei930@sohu.com>
2022-03-19 17:23:10 +0800

    first commit

---
+++ src/main/java/wiki/liulei/git/GitTest.java
@@ -0,0 +1,11 @@
+package wiki.liulei.git;
+
+/**
+ * @author 刘磊
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
